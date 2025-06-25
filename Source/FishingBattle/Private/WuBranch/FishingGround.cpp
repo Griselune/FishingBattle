40f01d96db5db6f1685865b2e02c5d6a22bef657
gene <cg100111@gmail.com>
2025-06-25 21:27:08 +0900

    釣り場のベースアクターを作った

---
+++ Source/FishingBattle/Private/WuBranch/FishingGround.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/FishingGround.h"
+
+// Sets default values
+AFishingGround::AFishingGround()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFishingGround::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFishingGround::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
