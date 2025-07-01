58d1119dc365db00b765973450d6ada53de96714
0315-pc <mocochankawaii00110953@gmail.com>
2025-07-01 12:25:43 +0900

    武器のクラス制作
    武器の基底クラスを制作
    テスト武器の制作
    BP_HitTestを制作

---
+++ Source/FishingBattle/Public/TakimotoBranch/CPPWeaponTest1.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TakimotoBranch/CPPBaseWeapon.h"
+#include "CPPWeaponTest1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHINGBATTLE_API ACPPWeaponTest1 : public ACPPBaseWeapon
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Attack_Implementation() override;
+
+	virtual void OnHit_Implementation(AActor* HitActor) override;
+};
---
+++ Source/FishingBattle/Public/TakimotoBranch/CPPWeaponType.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPPWeaponType.generated.h"
+
+UENUM(BlueprintType)
+enum class ECPPWeaponType : uint8
+{
+	Non,
+	WeaponTest1,
+	WeaponTest2,
+	WeaponTest3
+};
