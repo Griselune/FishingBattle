2920a87093131236baaa605e33d4f0292621a2b1
0315-pc <mocochankawaii00110953@gmail.com>
2025-07-02 12:12:34 +0900

    アイテムの基底クラスを作ったよ。
    クラッシュしているプロジェクトを他の人が起動したらどうなるのかのテストもかねて。

--- Source/FishingBattle/Private/TakimotoBranch/CPPBaseItem.cpp
+++ Source/FishingBattle/Private/TakimotoBranch/CPPBaseItem.cpp
@@ -1,11 +1,10 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "TakimotoBranch/CPPBaseWeapon.h"
-#include "Kismet/GameplayStatics.h"
+#include "TakimotoBranch/CPPBaseItem.h"
 
 // Sets default values
-ACPPBaseWeapon::ACPPBaseWeapon()
+ACPPBaseItem::ACPPBaseItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
@@ -19,20 +18,4 @@ ACPPBaseWeapon::ACPPBaseWeapon()
 	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
 	BoxCollision->SetupAttachment(RootComponent);
 
-	Damage = 0.f;
-
-	WeaponType = ECPPWeaponType::Non;
-}
-
-void ACPPBaseWeapon::Attack_Implementation()
-{
-
-}
-
-void ACPPBaseWeapon::OnHit_Implementation(AActor* HitActor)
-{
-	if (HitActor && HitActor != this) {
-		UGameplayStatics::ApplyDamage(HitActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
-		UE_LOG(LogTemp, Error, TEXT("Hit!"));
-	}
 }
