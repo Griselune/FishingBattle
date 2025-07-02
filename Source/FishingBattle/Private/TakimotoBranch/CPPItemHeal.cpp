2a696ecfcbbeaff6b226cb3dda2ed045e986b70d
0315-pc <mocochankawaii00110953@gmail.com>
2025-07-02 23:49:21 +0900

    回復アイテムを作成
    プレイヤーと衝突したらHealthの値を増やすアクタを追加しました。

--- Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
+++ Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
@@ -1,5 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "TakimotoBranch/CPPItemHeal.h"
+#include "FishingBattle/FishingBattleCharacter.h"
 
+void ACPPItemHeal::Heal(AActor* Player)
+{
+	AFishingBattleCharacter* fbc = Cast<AFishingBattleCharacter>(Player);
+	if (fbc)
+	{
+		fbc->Health += 10.f;
+		UE_LOG(LogTemp, Error, TEXT("Heal"));
+
+		this->Destroy();
+	}
+}
--- Source/FishingBattle/Public/TakimotoBranch/CPPItemHeal.h
+++ Source/FishingBattle/Public/TakimotoBranch/CPPItemHeal.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,4 +14,8 @@ class FISHINGBATTLE_API ACPPItemHeal : public ACPPBaseItem
 {
 	GENERATED_BODY()
 	
+protected:
+
+	UFUNCTION(BlueprintCallable, Category = "Item")
+	void Heal(AActor* Player);
 };
