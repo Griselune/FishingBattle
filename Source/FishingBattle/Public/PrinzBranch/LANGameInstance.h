c4b9977792bd21b67adaebcc67de3d84909d5d53
Griselune <philoch74@gmail.com>
2025-07-02 19:56:13 +0900

    GameInstanceに通信のデータを持たせる変数を追加

--- Source/FishingBattle/Public/PrinzBranch/LANGameInstance.h
+++ Source/FishingBattle/Public/PrinzBranch/LANGameInstance.h
@@ -17,6 +17,24 @@ class FISHINGBATTLE_API ULANGameInstance : public UGameInstance
 
 public:
 
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    FString HostSessionName;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    FString HostSessionPassword;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    int32 HostSessionPlayerLimit;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    int32 HostSessionTimeLimit;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    bool isDeathMatch;
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Shared Data")
+    bool isBattleRoyale;
+
     UFUNCTION(BlueprintCallable, Category = "LAN Session")
     void CreateLANSession(const FString& SessionName, const FString& Password);
 
