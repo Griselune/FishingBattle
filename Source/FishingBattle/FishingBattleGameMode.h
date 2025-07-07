5ac9f996f9e864a3352a65e298366aa39089ff13
gene <cg100111@gmail.com>
2025-07-07 11:16:38 +0900

    試しにランダムの位置に釣り場を生成してみた(未完成)

--- Source/FishingBattle/FishingBattleGameMode.h
+++ Source/FishingBattle/FishingBattleGameMode.h
@@ -1,10 +1,14 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "FishingBattleGameMode.generated.h"
+
+// 2025.07.06 ウー start
+class AFishingGround;
+// 2025.07.06 ウー end
 
 UCLASS(minimalapi)
 class AFishingBattleGameMode : public AGameModeBase
@@ -14,11 +18,48 @@ class AFishingBattleGameMode : public AGameModeBase
 public:
 	AFishingBattleGameMode();
 
+	// 2025.07.06 ウー start
+	virtual void StartPlay() override;
+	// 2025.07.06 ウー end
+
 	/// <summary>
-	/// �v���C���[���X�|�[��
+	/// プレイヤーリスポーン
 	/// </summary>
 	/// <param name="Controller"></param>
 	void RespawnPlayer(AController* Controller);
+	
+private:
+
+	// 2025.07.06 ウー start
+	
+	/// <summary>
+	/// ランダムの位置に釣り場を生成する
+	/// </summary>
+	void SpawnFishingGroundRandom();
+
+	/// <summary>
+	/// 釣り場のサンプル
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Fishing", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<AFishingGround> FishingGroundSample;
+
+	/// <summary>
+	/// 一ゲーム内で釣り場の数
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Fishing", meta = (AllowPrivateAccess = "true"))
+	int FishingGroundNums;
+
+	/// <summary>
+	/// 釣り場の生成範囲
+	/// </summary>
+	TObjectPtr<class AFishingGroundSpawnArea> FishingGroundSpawnArea;
+
+	/// <summary>
+	/// 釣り場
+	/// </summary>
+	TArray<AFishingGround*> Grounds;
+	// 2025.07.06 ウー end
+
 };
 
 
