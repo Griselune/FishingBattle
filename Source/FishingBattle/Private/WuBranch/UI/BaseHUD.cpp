c13d63c661e4c66d77ccf1a54134cc8c3de5fb76
gene <cg100111@gmail.com>
2025-07-08 10:27:17 +0900

    HUDを追加した

---
+++ Source/FishingBattle/Private/WuBranch/UI/BaseHUD.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/UI/BaseHUD.h"
+#include "Blueprint/UserWidget.h"
+
+ABaseHUD::ABaseHUD()
+{
+}
+
+void ABaseHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AddWidgetToView();
+}
+
+void ABaseHUD::AddWidgetToView()
+{
+	if (HUDUIClass)
+	{
+		HUDWidget = CreateWidget<UUserWidget>(GetWorld(), HUDUIClass);
+		if (HUDWidget)
+		{
+			HUDWidget->AddToViewport();
+			HUDWidget->SetVisibility(ESlateVisibility::HitTestInvisible);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("HUDWidget is null!"));
+		}
+	}
+}
