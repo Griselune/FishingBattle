3c05b130c618e3689f4c7f92b5b50e69653a150b
0315-pc <mocochankawaii00110953@gmail.com>
2025-07-24 15:16:49 +0900

    回復アイテム変更
    Heal関数がサーバーとクライアントの二回呼ばれているっぽい(未解決　2025/07/24 15:16)

--- Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
+++ Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
@@ -6,15 +6,18 @@
 
 void ACPPItemHeal::Heal(AActor* Player)
 {
-	AFishingBattleCharacter* fbc = Cast<AFishingBattleCharacter>(Player);
-	if (fbc)
-	{
-		// 2025.07.17 ウー start
-		//fbc->Health += 10.f;
-		fbc->Heal(10.0f);
-		// 2025.07.17 ウー end
-		UE_LOG(LogTemp, Error, TEXT("Heal"));
+	if (IsHeal == false) {
+		IsHeal = true;
+		AFishingBattleCharacter* fbc = Cast<AFishingBattleCharacter>(Player);
+		if (fbc)
+		{
+			// 2025.07.17 ウー start
+			//fbc->Health += 10.f;
+			fbc->Heal(10.0f);
+			// 2025.07.17 ウー end
+			UE_LOG(LogTemp, Error, TEXT("Heal"));
 
-		this->Destroy();
+			this->Destroy();
+		}
 	}
 }
