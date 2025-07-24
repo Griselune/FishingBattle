6139c376d269e27e04660d633ec3c6c55460f47e
gene <cg100111@gmail.com>
2025-07-24 14:46:52 +0900

    プリンス君の代わりにプッシュする

---
+++ Source/FishingBattle/Public/PrinzBranch/MenuPlayerController.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MenuPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHINGBATTLE_API AMenuPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(Server, Reliable)
+	void GetDataFromServer();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+};
