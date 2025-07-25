50d6ba9ce68895f80bc70406d90d40f079bfc78f
0315-pc <mocochankawaii00110953@gmail.com>
2025-07-25 13:51:40 +0900

    色々修正

--- Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
+++ Source/FishingBattle/Private/TakimotoBranch/CPPItemHeal.cpp
@@ -11,6 +11,7 @@ void ACPPItemHeal::Heal(AActor* Player)
 		AFishingBattleCharacter* fbc = Cast<AFishingBattleCharacter>(Player);
 		if (fbc)
 		{
+			if (!HasAuthority()) return;
 			// 2025.07.17 ウー start
 			//fbc->Health += 10.f;
 			fbc->Heal(10.0f);
