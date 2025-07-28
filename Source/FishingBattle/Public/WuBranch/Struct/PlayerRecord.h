b812dd2085b2bdae3b8be5c2b942fd73e62b49b1
gene <cg100111@gmail.com>
2025-07-28 17:52:22 +0900

    プレイヤーの記録内容

---
+++ Source/FishingBattle/Public/WuBranch/Struct/PlayerRecord.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerRecord.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FPlayerRecord
+{
+	GENERATED_BODY()
+
+	/// <summary>
+	/// プレレイヤーID
+	/// </summary>
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Player Record")
+	int32 ID;
+
+	/// <summary>
+	/// 死亡回数
+	/// </summary>
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Player Record")
+	int DeathCount;
+
+	/// <summary>
+	/// 与えたダメージの合計
+	/// </summary>
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "Player Record")
+	float TotalDamage;
+};
