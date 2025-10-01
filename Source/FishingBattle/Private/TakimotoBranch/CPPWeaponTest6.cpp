2f8b6818bd438eb91a6058e3d772aeec0e623e69
0315-pc <mocochankawaii00110953@gmail.com>
2025-10-01 09:57:31 +0900

    魚(武器)の種類を増やした
    3種類だった魚を7種類に増やしました。

---
+++ Source/FishingBattle/Private/TakimotoBranch/CPPWeaponTest6.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TakimotoBranch/CPPWeaponTest6.h"
+
+void ACPPWeaponTest6::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACPPWeaponTest6::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void ACPPWeaponTest6::Attack_Implementation()
+{
+
+}
+
+void ACPPWeaponTest6::OnHit_Implementation(AActor* HitActor)
+{
+	Super::OnHit_Implementation(HitActor);
+}
+
+void ACPPWeaponTest6::Attack_Begin_Implementation()
+{
+	Super::Attack_Begin_Implementation();
+}
+
+void ACPPWeaponTest6::Attack_Finish_Implementation()
+{
+	Super::Attack_Finish_Implementation();
+}
---
+++ Source/FishingBattle/Public/TakimotoBranch/CPPWeaponTest7.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TakimotoBranch/CPPBaseWeapon.h"
+#include "CPPWeaponTest7.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHINGBATTLE_API ACPPWeaponTest7 : public ACPPBaseWeapon
+{
+	GENERATED_BODY()
+	
+protected:
+
+	virtual void Tick(float DeltaSeconds);
+
+public:
+
+	virtual void Attack_Implementation() override;
+
+	virtual void OnHit_Implementation(AActor* HitActor) override;
+
+	virtual void Attack_Begin_Implementation() override;
+
+	virtual void Attack_Finish_Implementation() override;
+
+	virtual void BeginPlay() override;
+};
